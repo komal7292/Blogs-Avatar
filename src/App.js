b571845ccb1d5443d234e2209596da74ade9236d
komal <kg681278@gmail.com>
2021-05-24 12:53:00 +0530

    completed this session

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author='Sam' time='Todat at 7:10PM' content='Nice blog post!'/>
-      <CommentDetail author='Mac' time='Just now 9:10PM' content='Informative post!'/>
-      <CommentDetail author='Jam' time='Yesterday at 8:10AM' content='I like the subject!'/>
+        <CommentDetail
+          author="Sam"
+          time="Todat at 7:10PM"
+          content="Nice blog post!"
+        />
+
+      <CommentDetail
+        author="Mac"
+        time="Just now 9:10PM"
+        content="Informative post!"
+      />
+      <CommentDetail
+        author="Jam"
+        time="Yesterday at 8:10AM"
+        content="I like the subject!"
+      />
     </div>
   );
-}
+};
 export default App;
